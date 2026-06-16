df369f8c27e6328f06ab1dac75034d6fa4c547ec
zlavrov <zakhar.polosky@gmail.com>
2026-06-16 13:47:57 +0300

    redirect link

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
-// header("Location: https://sgenerator.space/public/index.php");
-header("Location: http://192.168.0.193:8080/public/index.php");
+header("Location: https://sgenerator.space/public/index.php");
 ?>
