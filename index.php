711856abb9c4dd50180c346689533dd3d5ecd034
zlavrov <zakhar.polosky@gmail.com>
2026-06-16 22:54:49 +0300

    create home page

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-
-header("refresh: 1; http://192.168.0.193:8080/public/index.php");
+// header("refresh: 1; http://192.168.0.193:8080/public/index.php");
+header("Location: /public/index.php");
 
 	echo '<title>Symfony Installed</title><div style="background: #e9ffed; border: 1px solid #b0dab7; padding: 15px;" align="center" >
 	<font size="5" color="#182e7a">Symfony is installed successfully.</font><br /><br />
@@ -11,14 +11,4 @@ header("refresh: 1; http://192.168.0.193:8080/public/index.php");
 
 Symfony is aimed at building robust applications in an enterprise context. This means that you have full control over the configuration: from the directory structure to the foreign libraries, almost everything can be customized. </font></div>';
 
-
-// header("refresh: 1; https://sgenerator.space/public/index.php");
-
-// 	echo '<title>Symfony Installed</title><div style="background: #e9ffed; border: 1px solid #b0dab7; padding: 15px;" align="center" >
-// 	<font size="5" color="#182e7a">Symfony is installed successfully.</font><br /><br />
-// 	<font size="4">Symfony is a Framework and doesn\'t have an index page.<br /><br />
-// 	You will be redirected to its "public" folder in 5 seconds...<br /><br />
-// 	Symfony aims to speed up the creation and maintenance of web applications, and to replace the repetitive coding tasks by power, control and pleasure.
-
-// Symfony is aimed at building robust applications in an enterprise context. This means that you have full control over the configuration: from the directory structure to the foreign libraries, almost everything can be customized. </font></div>';
 ?>
